cfb7995ac243261f501d3ce1a604aaea9aa65535
Fadhlin Nauri <fadhlin227@gmail.com>
2026-05-31 19:37:53 +0700

    Fix again

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,8 +4,6 @@ const authController = require('../controllers/authController');
 
 router.post('/register', authController.register);
 router.get('/', authController.getKaryawan);
-
-// Tambahkan baris ini untuk rute login
 router.post('/login', authController.login);
 
 module.exports = router;
