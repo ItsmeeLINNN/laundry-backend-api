71b0e33f6f694e61bf024f0e9f4a713a087d4f6a
Fadhlin Nauri <fadhlin227@gmail.com>
2026-06-07 06:10:12 +0700

    Tambah rute pesanan

--- routes/pesananRoutes.js
+++ routes/pesananRoutes.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 const pesananController = require('../controllers/pesananController');
 
-// Rute untuk membuat pesanan baru
+// 1. Rute untuk membuat pesanan baru (POST)
 router.post('/', pesananController.buatPesanan);
+
+// 2. Rute untuk mengambil daftar pesanan (GET) - INI YANG TADI HILANG
+router.get('/', pesananController.getListPesanan);
+
+// 3. Rute untuk mengubah status pesanan (PUT)
+router.put('/:id/status', pesananController.updateStatus);
 
 module.exports = router;
